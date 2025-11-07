bdf0c4d758be3ecafb5065f724caf217b43ea2c6
Alessandro <alessandroporcheddu000@gmail.com>
2025-11-07 12:08:27 +0100

    fixed more navigation

--- src/config/index.js
+++ src/config/index.js
@@ -75,7 +75,6 @@ export const RESUME_DOWNLOAD_NAME = "AlessandroPorcheddu-Resume.pdf";
 // ========== NAVIGATION ==========
 export const NAV_LINKS = [
   { id: "about", label: "About" },
-  { id: "skills", label: "Skills" },
   { id: "experience", label: "Experience" },
   { id: "education", label: "Education" },
   { id: "projects", label: "Projects" },
