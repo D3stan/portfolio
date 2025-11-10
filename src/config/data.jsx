b892655f5a990a3f7857062d5d961e280693c5c2
Alessandro <alessandroporcheddu000@gmail.com>
2025-11-10 21:17:57 +0100

    minor fixes

--- src/config/data.jsx
+++ src/config/data.jsx
@@ -34,8 +34,8 @@ export const PROJECTS_FEATURED = [
     title: "JavaDyno",
     subtitle: "Engine Dynamometer Management Software",
     blurb:
-      "Developed an engine dynamometer management system (EDMS) with a group project team, overseeing complete data retrieval and graphical plotting. Implemented an MVC core structure for data exportation and session logging. Ensured OBD-II CAN bus compatibility for vehicle testing, enhancing real-world applicability. Integrated WebSocket and Serial connection support for versatile device communication.",
-    tech: ["Java", "MVC", "WebSocket", "Serial", "OBD-II", "CAN Bus"],
+      "Guided a group of 4 in developing an engine dynamometer management system (EDMS) with a group project team, overseeing complete data retrieval and graphical plotting. Implemented an MVC core structure for data exportation and session logging. Ensured OBD-II CAN bus compatibility for vehicle testing, enhancing real-world applicability. Integrated WebSocket and Serial connection support for versatile device communication.",
+    tech: ["Java", "MVC", "WebSocket", "Serial", "OBD-II", "CANBUS"],
     image: "/images/projects/featured/javadyno.png",
     video: null,
     repo: "https://github.com/MattechIT/OOP24-java-dyno",
@@ -54,7 +54,6 @@ export const PROJECTS_FEATURED = [
     tech: [
       "ESP32",
       "WebSocket",
-      "WiFi",
       "KiCad",
       "Fusion360",
       "Node.js",
@@ -77,7 +76,7 @@ export const PROJECTS_SMALL = [
     id: 3,
     title: "Motorcycle Parts E-commerce Platform",
     blurb:
-      "Built a full-stack e-commerce platform using Laravel 12, React 19, and Inertia.js. Implemented secure payment processing with Stripe via Laravel Cashier. Designed responsive mobile UI with reusable components using the shadcn React library. Developed an admin dashboard for product, order, and customer management with analytics and reporting tools. Ensured robust security with mandatory email and webhook signature validation.",
+      "Built a full-stack e-commerce platform using Laravel, React, and Inertia.js. Implemented secure payment processing with Stripe via Laravel Cashier. Developed an admin dashboard for product, order, and customer management with analytics and reporting tools. Ensured robust security with mandatory email and webhook signature validation. Deployed using Linux VPS.",
     tech: ["Laravel 12", "React 19", "Inertia.js", "Stripe", "Shadcn"],
     image: "/images/projects/small/motorcycle-ecommerce.png",
     video: null,
@@ -144,7 +143,7 @@ export const SCHOOLS = [
   {
     school: "University of Bologna",
     degree: "B.Sc. in Computer Science and Engineering",
-    period: "Sept. 2023 - Jul. 2026",
+    period: "Sept. 2023 - Present",
     badge: "UNIBO",
     address: "Cesena, Italy",
     url: "https://www.unibo.it",
